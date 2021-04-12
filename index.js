82cebdef13b2f74a778aca68a891bd09b7a6b4ca
Omer Sabah <omerrj99@gmail.com>
2021-04-12 12:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 document.getElementById('main').remove();
 const newHeader=document.createElement('h1');
 newHeader.setAttribute('id','victory');
+newHeader.innerHTML='Omer is the champion'
 document.body.append(newHeader);
 
