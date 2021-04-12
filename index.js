c51c64e4a9463c518056d7298783ec77b880684e
Omer Sabah <omerrj99@gmail.com>
2021-04-12 11:57:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+const newHeader=document.createElement('H1').setAttribute('id','victory');
+newHeader.innerHtml=
+document.body.appendChild(newHeader);
